c46689a4713b7dc8a2a2827e5509a3c7d741b1f8
Andrey Ignashov <maylo7771@yandex.ru>
2024-10-05 19:19:19 +0300

    Добавлена типизация, начата документация

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+export interface IProductList {
+	total: number;
+	items: IProductItem[];
+}
+
+export interface IProductItem {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number;
+}
+export type TProductInCatalog = Omit<IProductItem, 'description'>;
+export type TProductInBasket = Pick<IProductItem, 'id' | 'title' | 'price'>;
+
+export type TPaymentOption = 'card' | 'cash' | '';
+
+export interface IOrder {
+	payment: TPaymentOption;
+	email: string;
+	phone: string;
+	address: string;
+	total: number;
+	items: string[];
+}
+export type TOrder = Omit<IOrder, 'items' | 'total'>;
+
+export type TBasket = Pick<IOrder, 'items' | 'total'>
+
+export interface IOrderSuccess {
+	id: string;
+	total: number;
+}
+
+export interface NotFoundItem {
+	error: string;
+}
